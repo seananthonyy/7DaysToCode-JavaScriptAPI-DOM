88a8979934848b3ca38ba54766f83dd49847563c
Antonio Oliveira <antoniopx12@gmail.com>
2024-01-28 14:57:47 -0300

    Dia 3 - Implementa API e mudanças na fillMovies

--- main.js
+++ main.js
@@ -1,66 +1,56 @@
+import { key } from "./apikey.js";
+
 const moviesContainer = document.querySelector('.movies')
 
-const movies = [
-    {
-        image: 'https://img.elo7.com.br/product/original/3FBA809/big-poster-filme-batman-2022-90x60-cm-lo002-poster-batman.jpg',
-        title: 'Batman',
-        rating: 9.2,
-        year: 2022,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        isFavorited: false
-    },
-    {
-        image: 'img/Interstellar_Filme.png',
-        title: 'Interstellar',
-        rating: 10.0,
-        year: 2014,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        isFavorited: true
-    },
-    {
-        image: 'https://upload.wikimedia.org/wikipedia/en/1/17/Doctor_Strange_in_the_Multiverse_of_Madness_poster.jpg',
-        title: 'Doctor Strange',
-        rating: 9.2,
-        year: 2022,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        isFavorited: true
-    },
-]
+async function getPopularMovies() {
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=pt-BR&page=1`
+    const fetchResponse = await fetch(url)
+    const { results } = await fetchResponse.json()
+    return results
+} 
 
-    window.onload = function () {
-        movies.forEach(movie => fillMovies(movie))
-    }
+window.onload = async function () {
+    const movies = await getPopularMovies()
+    movies.forEach(movie => fillMovies(movie))
+}
 
 function fillMovies(movie) {
 
-    const { title, image, rating, year, description, isFavorited } = movie
+    const { title, poster_path, vote_average, release_date, overview } = movie
+    const isFavorited = false
 
+    // movie
     const movieElement = document.createElement('div')
     movieElement.classList.add('movie')
     moviesContainer.appendChild(movieElement)
 
+    // movie-info
     const movieInfo = document.createElement('div')
     movieInfo.classList.add('movie-info')
 
+    // movie-image
     const movieImageContainer = document.createElement('div')
     movieImageContainer.classList.add('movie-image')
     const movieImage = document.createElement('img')
-    movieImage.src = image
+    movieImage.src = `https://image.tmdb.org/t/p/w500${poster_path}`
     movieImage.alt = `${title} Poster`
     movieImageContainer.appendChild(movieImage)
     movieInfo.appendChild(movieImageContainer)
 
+    // movie-text
     const movieTextContainer = document.createElement('div')
     movieTextContainer.classList.add('movie-text')
     const movieTitle = document.createElement('h4')
-    movieTitle.textContent = `${title} (${year})`
+    movieTitle.textContent = `${title} (${release_date.split("-")[0]})`
     movieTextContainer.appendChild(movieTitle)
     movieInfo.appendChild(movieTextContainer)
 
+    // movie-text movie-info
     const informations = document.createElement('div')
     informations.classList.add('movie-info')
     movieTextContainer.appendChild(informations)
 
+    // rating movie-rate
     const ratingContainer = document.createElement('div')
     ratingContainer.classList.add('rating')
     const starImage = document.createElement('img')
@@ -68,11 +58,12 @@ function fillMovies(movie) {
     starImage.alt = 'Star'
     const movieRate = document.createElement('span')
     movieRate.classList.add('movie-rate')
-    movieRate.textContent = rating
+    movieRate.textContent = vote_average
     ratingContainer.appendChild(starImage)
     ratingContainer.appendChild(movieRate)
     informations.appendChild(ratingContainer)
 
+    // favorite favoriteImage movie-favorite
     const favorite = document.createElement('div')
     favorite.classList.add('favorite')
     const favoriteImage = document.createElement('img')
@@ -86,10 +77,11 @@ function fillMovies(movie) {
     favorite.appendChild(favoriteText)
     informations.appendChild(favorite)
 
+    // movie-description
     const movieDescriptionContainer = document.createElement('div')
     movieDescriptionContainer.classList.add('movie-description')
     const movieDescription = document.createElement('span')
-    movieDescription.textContent = description
+    movieDescription.textContent = overview
     movieDescriptionContainer.appendChild(movieDescription)
 
     movieElement.appendChild(movieInfo)
